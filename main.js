dcdba3ef7d182f0b1d3ade55bc3ad5863b7b1b4c
Kristian Brockmann <kbrockmann@bigpond.com>
2023-10-03 09:20:31 +1100

    Create main.js
    A Standard "main.js" header template. For ease of reference for effective software development projects.

---
+++ main.js
@@ -0,0 +1,12 @@
+/**************************************************************************
+* Title: "Standard"
+* Author: K.Brockmann
+* Date Created:
+* Date Modified:
+* Acknowledgements: Education, Standards Australia, ABC
+* Description: "Standard js file."
+**************************************************************************/
+
+"use strict";
+
+// Detail here... Link to index.html and style.css.
