e011d3b7ad59f1f14a42648cd93cd129176a2efd
brockyKris <100324743+brockyKris@users.noreply.github.com>
2023-10-02 12:21:13 +1100

    Create temp_humidity.py
    Pseudo code, for python interpretation for a temperature and humidity sensor for the Raspberry Pi.

---
+++ temp_humidity.py
@@ -0,0 +1,10 @@
+/********************************************
+* Python file to Log Temperature & Humidity
+********************************************/
+
+// import devices?
+// import sleep from Time??
+// write the calibration of the sensor
+// read the sensor
+// configure the data to logical output
+// write to a log file - sleep for 1 hour for example
